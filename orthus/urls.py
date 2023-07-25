c2c8fa2c77449c38c6e9b59e20f72042a1439f5d
Tero Tervala <tero.tervala@unikie.com>
2023-07-25 09:07:11 +0300

    Add Django rest framework
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- orthus/urls.py
+++ orthus/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+admin.site.site_header = "Orthus Administration"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-	import debug_toolbar
-	urlpatterns = [ path('__debug__/', include(debug_toolbar.urls)) ] + urlpatterns
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
