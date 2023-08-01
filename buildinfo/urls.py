f02f638587281607fbf627cf40facb0434083e04
Tero Tervala <tero.tervala@unikie.com>
2023-08-01 13:06:17 +0300

    Use Viewsets instead of function based views
    Viewsets are much simpler to use than function based views.
    
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

--- buildinfo/urls.py
+++ buildinfo/urls.py
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.urls import path
+from django.urls.conf import include
+from rest_framework_nested import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('buildinfo', views.BuildinfoViewset)
+router.register('output_store_path', views.OutputStorePathViewset)
+router.register('downloadable', views.DownloadableViewset)
+
 urlpatterns = [
-    path('buildinfo/', views.buildinfo_list),
-    # path('buildinfo/<str:server>/<int:build_id>/', views.build_detail),
-    path('buildinfo/<int:pk>/', views.buildinfo_detail),
-    path('output_store_path/', views.output_store_path_list),
-    path('output_store_path/<int:pk>/', views.output_store_path_detail),
-    path('downloadable/', views.downloadable_list),
-    path('downloadable/<int:pk>/', views.downloadable_detail),
+    path('', include(router.urls)),
 ]
--- buildinfo/views.py
+++ buildinfo/views.py
@@ -1,56 +1,35 @@
 # SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
 # SPDX-License-Identifier: Apache-2.0
 
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from .models import BuildInfo, OutputStorePath, Downloadable
 from .serializers import BuildinfoSerializer, BuildinfoListSerializer, OutputStorePathSerializer, DownloadableSerializer
 
 
-@api_view(['GET', 'POST'])
-def buildinfo_list(request):
+class BuildinfoViewset(ModelViewSet):
     queryset = BuildInfo.objects.all()
-    serializer = BuildinfoListSerializer(queryset, many=True)
-    return Response(serializer.data)
+    serializer_class = BuildinfoSerializer
+
+    def get_serializer(self, *args, **kwargs):
+        if kwargs.get('many') == True:
+            return BuildinfoListSerializer(*args, **kwargs)
+        return super().get_serializer(*args, **kwargs)
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
-# @api_view()
-# def build_detail(request, server, build_id):
-#    return Response(f'OK\nserver={server}\nbuild_id={build_id}')
-
-
-@api_view(['GET', 'PUT', 'PATCH'])
-def buildinfo_detail(request, pk):
-    buildinfo = get_object_or_404(BuildInfo, pk=pk)
-    serializer = BuildinfoSerializer(buildinfo)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-def output_store_path_list(request):
+class OutputStorePathViewset(ModelViewSet):
     queryset = OutputStorePath.objects.all()
-    serialzer = OutputStorePathSerializer(queryset, many=True)
-    return Response(serialzer.data)
+    serializer_class = OutputStorePathSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
-@api_view(['GET', 'PUT', 'PATCH'])
-def output_store_path_detail(request, pk):
-    output_store_path = get_object_or_404(OutputStorePath, pk=pk)
-    serializer = OutputStorePathSerializer(output_store_path)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'POST'])
-def downloadable_list(request):
+class DownloadableViewset(ModelViewSet):
     queryset = Downloadable.objects.all()
-    serialzer = DownloadableSerializer(queryset, many=True)
-    return Response(serialzer.data)
+    serializer_class = DownloadableSerializer
 
-
-@api_view(['GET', 'PUT', 'PATCH'])
-def downloadable_detail(request, pk):
-    downloadable = get_object_or_404(Downloadable, pk=pk)
-    serializer = DownloadableSerializer(downloadable)
-    return Response(serializer.data)
+    def get_serializer_context(self):
+        return {'request': self.request}
