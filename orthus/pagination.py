63bf0a506e2b0bc2522208d695efdf095f99fb08
Tero Tervala <tero.tervala@unikie.com>
2023-08-01 10:51:32 +0300

    Add default pagination class and settings
    Signed-off-by: Tero Tervala <tero.tervala@unikie.com>

---
+++ orthus/pagination.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
+# SPDX-License-Identifier: Apache-2.0
+
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class DefaultPagination(LimitOffsetPagination):
+    pass
--- orthus/settings.py
+++ orthus/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from .pagination import DefaultPagination
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,4 +154,6 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_PAGINATION_CLASS': DefaultPagination,
+    'PAGE_SIZE': 50,
 }
